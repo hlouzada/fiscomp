7da9b8934d7b661d650116a3aceb925fe3579fa9
hlouzada <hendriklouzada@gmail.com>
2020-04-01 07:11:23 -0300

    fui burro dei git add na pasta errada, c ajeitada

--- projeto-2/tarefa-c/tarefa-c-11212400.f90
+++ projeto-2/tarefa-c/tarefa-c-11212400.f90
@@ -3,9 +3,8 @@ PROGRAM bebado3d
        CHARACTER (22) filename
        PARAMETER (Nmax=1000000)
        PARAMETER (pi=4e0*atan(1e0))
-       COMPLEX zpos
-       DIMENSION zpos(Nmax)
-       zpos = 0
+       DIMENSION pos(Nmax,2)
+       pos = 0
 
        WRITE(*,*) 'Digite o número N de passos (N<=10e6):'
        READ(*,*) N
@@ -19,16 +18,13 @@ PROGRAM bebado3d
        READ(*,*) M
 
        do i=1, N
-              x = 0
-              y = 0
               do j=1, M
                      arg = 2*pi*rand()
-                     x = x + int(cos(arg))
-                     y = y + int(sin(arg))
-                     zpos(j) = zpos(j) + cmplx(x,y) 
+                     pos(j,:) = pos(j,:) + (/cos(arg),sin(arg)/) 
               end do
        
 
+              r2 = 0
               rmedio = 0
               delta2 = 0
        
@@ -39,19 +35,16 @@ PROGRAM bebado3d
                      OPEN(10, file=filename)
               
                      do k = 1, M
-                            r = abs(zpos(k))
-                            rmedio = rmedio + r
-                            delta2 = delta2 + r**2
-                     
-                            WRITE(10,'(F0.1,", ",F0.1)') zpos(k)
+                            WRITE(10,'(I0,", ",I0)') int(pos(k,:))
                      end do
-              
-                     rmedio = rmedio/M
-                     delta2 = delta2/M - rmedio**2
-              
-                     WRITE(*,'("N: 10e",I0," <r>: ",F0.3," delta**2: ",F0.3)') int(exp), rmedio, delta2
-
                      CLOSE(10)
+
+                     r2 = (sum(pos(:,1)**2) + sum(pos(:,2)**2))
+                     
+                     rmedio = sqrt(r2)/M
+                     delta2 = r2/M - rmedio**2
+              
+                     WRITE(*,'("N: 10e",I0," <r>: ",1F0.3," delta**2: ",1F0.3)') int(exp), abs(rmedio), abs(delta2)
               end if
        end do
 
